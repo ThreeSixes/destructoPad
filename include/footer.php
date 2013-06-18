2a6953c8a4df515a46b555d6c8343e5e7b1e0d44
Josh Lucy <josh.lucy@zed.com>
2013-06-18 14:03:35 -0400

    Text change on the footer.

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,8 @@
 <br/>
-<br/>
-<span class="tinyText">DestructoPad is an open source project designed to run over the TOR network that will securely and temporarily store a message until it is read (and subsequently deleted) or it expires.</span>
+<span class="tinyText">DestructoPad is an open source project allows you to create short text notes
+(up to <?php //echo $dp->getOverheadSize; ?> bytes) and is designed to run over the TOR network as a hidden service.
+These notes or "pads" are stored on the server using encryption (<?php ?>). As soon as you create the pad you'll be given a URL
+which you can give to someone to read. As soon as they open the pad it is deleted from the server, and if pads aren't
+read they expire in <?php ?>. All pads are automatically encoded as UTF-8, and are returned with a text/plain MIME type.</span>
 </body>
 </html>
