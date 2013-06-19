73aee25c3692c7009e1daa102000af2530c4a6fc
Josh Lucy <josh@lucyindustrial.com>
2013-06-18 20:28:11 -0400

    Text changes.

--- include/footer.php
+++ include/footer.php
@@ -1,8 +1,9 @@
-<br/>
-<span class="tinyText">DestructoPad is an open source project allows you to create short text notes
-(up to <?php //echo $dp->getOverheadSize; ?> bytes) and is designed to run over the TOR network as a hidden service.
-These notes or "pads" are stored on the server using encryption (<?php ?>). As soon as you create the pad you'll be given a URL
-which you can give to someone to read. As soon as they open the pad it is deleted from the server, and if pads aren't
-read they expire in <?php ?>. All pads are automatically encoded as UTF-8, and are returned with a text/plain MIME type.</span>
-</body>
+        <br/>
+        <span class="tinyText">DestructoPad is an open source project allows you to create short text notes
+(up to <?php echo $dp->getMaxPadSize(); ?> bytes) and is designed to run over the TOR network as a hidden service.
+These notes or "pads" are stored on the server using encryption (<?php echo $dp->getEncryptionAlgo(); ?>). As soon as you create the pad you'll be given a URL
+which you can give to someone to read. As soon as they open the pad it is deleted from the server, and pads
+automatically expire between <?php $exprTime = $dp->getPadExpireRange(); echo $exprTime['min'] . " and " . $exprTime['max'];  ?> hours if they're not read.
+All pads are automatically encoded as UTF-8, and are returned with a text/plain MIME type.</span>
+    </body>
 </html>
