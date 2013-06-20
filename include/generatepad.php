85d67cbdbe386a9b55dfb7ed2c616d1ff1609439
Josh Lucy <josh.lucy@zed.com>
2013-06-20 12:46:11 -0400

    Removed/disabled debugging statements, modified some comments, and improved error reporting if the pad isn't added due to a recorded error.

--- include/generatepad.php
+++ include/generatepad.php
@@ -4,21 +4,17 @@
  * This file is designed to be an inline include that encrypts the pad hands it over to the data layer, and then presents the user with the URL.
  * By Josh Lucy <josh{AT}lucyindustrial_dawt_com>
  */
- echo "Activate error reporting... ";
- ini_set('display_errors',1); 
- error_reporting(E_ALL);
- echo "check pad length... ";
+
+ // Error display code...
+ //ini_set('display_errors', 0); 
+ //error_reporting(E_ALL);
+ 
  // Check the size of the post (in a rough fashion) to make sure we can use it.
  if (strlen($_POST['pad']) > 0 && strlen($_POST['pad']) < $dp->getMaxPadSize()) {
     
     // IF this has been satisfied let's load the data layer...
-    echo "Bring data layer... ";
     require("include/data.php");
     $dpdl = new destructoPadData(destructoPadData::DP_MODE_MYSQL);
-    
-    echo "Got data layer... ";
-    
-    //$dp = new destructoPad();
     
     // If our pad post isn't empty or too large, set the pad data.
     $padData = utf8_encode($_POST['pad']);   
@@ -35,17 +31,10 @@
     // Generate our encrypted text...
     $encrypted = $dp->getEncrypted($newGuid, $newURL, $padData);
     
-    echo "Got " . strlen($encrypted) . " encrypted bytes... ";
-    
-    echo "Check max padded size...";
     //Make sure our encrypted data doesn't violate the maximum size constraint.
     if (strlen($encrypted) <= $dp->getMaxPadSize()) {
-          // Call our data layer
-          echo "Calling addPad()... ";
-    
         // Now create the pad in the data layer
         $addWorked = $dpdl->addPad($dp->createHash($newURL), $expireTime, $encrypted);
-        echo "Called addPad()... ";
         
         // If the pad add worked, then let's give our user the good news and URL.
         if ($addWorked['success'] == TRUE) {
@@ -59,7 +48,7 @@
             echo "      <br />\n";
             echo "      <br />\n";
         } else {
-            echo "addPad() failed:: " . $addWorked['error'] . "... ";
+            echo "      <div class=\"warningText\">\n<span class=\"warningHead\">Unable to create pad</span><br />" . $addWorked['error'] . "\n</div>\n";
         }
     
     } else {
