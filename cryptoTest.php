fa7e4cd426ddaf4cb9c5976027b5cce8b395b451
Josh Lucy <josh@lucyindustrial.com>
2013-06-20 20:56:01 -0400

    Updated cryptoTest.php test cases to reflect the passing of the HMAC values from the getEncrypted() and getDecrypted() functions including displaying the HMAC values.

--- cryptoTest.php
+++ cryptoTest.php
@@ -16,7 +16,7 @@ $keyBText = "http://somesite.com/" . $keyAText; // Fake URL, including GUID
 $plainText = utf8_encode("http://www.youtube.com/watch?v=L-iepu3EtyE");
 
 $encryptedData = $dp->getEncrypted($keyAText, $keyBText, $plainText);
-$decryptedData = $dp->getDecrypted($keyAText, $keyBText, $encryptedData);
+$decryptedData = $dp->getDecrypted($keyAText, $keyBText, $encryptedData['encryptedBlock']);
 $encOverhead = $dp->getOverheadSize(strlen($plainText));
 
 echo "<<< BEGIN CRYPTO TESTS >>>\n\n";
@@ -25,9 +25,11 @@ echo "Original plaintext:   " . $plainText . "\n";
 echo "Key A Pre-hash:       " . $keyAText . "\n";
 echo "Key B Pre-hash:       " . $keyBText . "\n";
 echo "\n - Encryption - \n";
-echo "Encrypted Hex:        0x" . bin2hex($encryptedData) . "\n";
+echo "Encrypted Hex:        0x" . bin2hex($encryptedData['encryptedBlock']) . "\n";
+echo "Encrypted HMAC Hex:   0x" . bin2hex($encryptedData['HMAC']) . "\n";
 echo "\n - Decryption - \n";
 echo "Decrypted:            " . $decryptedData['plainText'] . "\n";
+echo "Decrypted HMAC Hex:   0x" . bin2hex($decryptedData['calcHMAC']) . "\n";
 echo "HMAC good:            ";
 echo ($decryptedData['hmacGood'] === TRUE) ? "Yes": "No";
 echo "\n";
@@ -39,10 +41,10 @@ echo "Original Hex:         0x" . bin2hex($plainText) . "\n";
 echo "\n - Stats -\n";
 echo "Original Size:        " . strlen($plainText) . " bytes\n";
 echo "Ciphertext Overhead:  " . $encOverhead . " bytes\n";
-echo "Encrypted Block Size: " . strlen($encryptedData) . " bytes\n";
+echo "Encrypted Block Size: " . strlen($encryptedData['encryptedBlock']) . " bytes\n";
 echo "Overhead + Plaintext: " . ($encOverhead + strlen($plainText)) . " bytes\n";
 echo "Block size correct:   ";
-echo (strlen($encryptedData) === ($encOverhead + strlen($plainText))) ? "Yes" : "No";
+echo (strlen($encryptedData['encryptedBlock']) === ($encOverhead + strlen($plainText))) ? "Yes" : "No";
 echo "\n";
 echo "\n<<< END CRYPTO TESTS >>>\n";
 ?>
