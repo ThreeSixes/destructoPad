723ec23bb0eadabc1b78c7aefe96888ea857409d
Josh Lucy <josh@lucyindustrial.com>
2013-06-21 02:34:22 -0400

    Multiple edits, starting over with sproc run.

--- include/getpad.php
+++ include/getpad.php
@@ -14,14 +14,14 @@
  
  // Check the size of the post (in a rough fashion) to make sure we can use it.
  if (strlen($_GET['targetPad']) > 0) {
-    // Set up or destructoPad instance...
-    require("include/destructoPad.php");
-    $dp = new destructoPad();
-    
     // Get our hash from the URL...
-    // DUMMY CODE HERE FOR NOW
-    $targetURL = "";
-    $targetGUID = ""; // URL GUID
+    $targetURL = "http://" . $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+    
+    // Get the parts before and after =
+    $urlParts = explode("=", $targetURL, 2);
+    
+    // After extraction, set it.
+    $targetGUID = $urlParts[1]; // URL GUID
     
     // Get our keys...
     $keyA = $dp->createHash($targetURL);
@@ -32,7 +32,8 @@
     $dpdl = new destructoPadData(destructoPadData::DP_MODE_MYSQL);
     
     // Look up our pad...
-    $gotPad = $dpdl->getPad($keyA);
+    $gotPad = $dpdl->getPad($dp->createHash($keyA));
+    
     
     // Did we get a pad?
     if($gotPad['success'] === TRUE) {
@@ -45,7 +46,7 @@
             $mimeHeader = $dp->getMimeAndCharset();
             
             // Build our HTTP response header...
-            $respHeader = "Content-Type: " . $mimeHeader['mime'] . "; charset=" . $mimeHeader['charset']; // FIX ME!
+            $respHeader = "Content-Type: " . $mimeHeader['mime'] . "; charset=" . $mimeHeader['charset'];
             
             // Set the HTTP MIME type header
             header($respHeader);
@@ -67,7 +68,13 @@
     else {
         // Present error page.
         include("include/header.php");
-        echo "      <div class=\"warningText\">\n<span class=\"warningHead\">Couldn't find the pad!</span><br />The requested pad either does not exist or has already been read and burned.\n</div>\n";
+        if (
+            $gotPad['error'] !== NULL) {
+            echo "      <div class=\"warningText\">\n<span class=\"warningHead\">Couldn't get the pad!</span><br />" . $gotPad['error'] . "\n</div>\n";
+        }
+        else{
+            echo "      <div class=\"warningText\">\n<span class=\"warningHead\">Couldn't find the pad!</span><br />The requested pad either does not exist or has already been read and burned.\n</div>\n";
+        }
         include("include/footer.php");
     }
     
